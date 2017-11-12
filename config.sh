6765b4e1d6238dcccc483625bcc08178c69aa839
John Ouellet <labboy0276@yahoo.com>
2017-11-12 17:12:06 -0500

    Update config

--- config.sh
+++ config.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+mkdir ~/lando/chiro/config/temp
+cp -r ~/lando/chiro/config/sync/. ~/lando/chiro/config/temp
+cd ~/lando/chiro
+lando drush cex -y
+rm -rf ~/lando/chiro/web/profiles/chiro_base/config/install
+mkdir ~/lando/chiro/web/profiles/chiro_base/config/install
+cp -r ~/lando/chiro/config/sync/. ~/lando/chiro/web/profiles/chiro_base/config/install
 find ~/lando/chiro/web/profiles/chiro_base/config/install/ -type f -exec sed -i -e '/^uuid: /d' {} \;
 find ~/lando/chiro/web/profiles/chiro_base/config/install/ -type f -exec sed -i -e '/_core/,+1d' {} \;
 rm -rf ~/lando/chiro/web/profiles/chiro_base/config/install/core.extension.yml
 rm -rf ~/lando/chiro/web/profiles/chiro_base/config/install/file.settings.yml
-rm -rf ~/lando/chiro/web/profiles/chiro_base/config/install/update.settings.yml
+rm -rf ~/lando/chiro/web/profiles/chiro_base/config/install/core.extension.yml
+rm -rf ~/lando/chiro/web/profiles/chiro_base/config/install/README.txt
+rm -rf ~/lando/chiro/config/sync
+mv ~/lando/chiro/config/temp ~/lando/chiro/config/sync
